f565ea328ddfc4b64e8a872628089aa8a434d04b
priimakmaks2404-png <priimakmaks2404@gmail.com>
2026-06-06 14:39:16 +0300

    end

--- 04_anim-20260516T122910Z-3-001/04_anim/main.py
+++ 04_anim-20260516T122910Z-3-001/04_anim/main.py
@@ -9,6 +9,7 @@ from kivy.uix.image import Image
 from kivy import platform
 from kivy.properties import NumericProperty
 from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
 
 
 class Menu(Screen):
@@ -22,6 +23,7 @@ class Menu(Screen):
 
     # Перехід до екрана налаштувань
     def go_settings(self, *args):
+        app.previous_screen = "menu"
         self.manager.current = "settings"
         self.manager.transition.direction = "up"
 
@@ -34,10 +36,14 @@ class Settings(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    # Повернення до меню
-    def go_menu(self, *args):
-        self.manager.current = "menu"
-        self.manager.transition.direction = "down"
+    # Повернення до попереднього екрана
+    def go_back(self, *args):
+        previous = getattr(app, 'previous_screen', 'menu')
+        if previous == "game":
+            app.returning_to_game_from_settings = True
+        direction = "down" if previous == "menu" else "down"
+        self.manager.current = previous
+        self.manager.transition.direction = direction
 
 
 # Клас для обертання картинок; в класі, який спадковує потрібно дадати властивість angle
@@ -76,9 +82,19 @@ class Fish(RotatedImage):
 
         swim.bind(on_complete=lambda w, a: setattr(self, "interaction_block", False))
 
+    # КЛІК!
+    def go_settings(self, *args):
+        app.previous_screen = "game"
+        self.manager.current = "settings"
+        self.manager.transition.direction = "up"
+
     # Перемогли рибу :)
     def defeated(self):
         self.interaction_block = True
+        
+        # Воспроизводим звук поражения рыбы
+        app.play_sound(app.sound_defeat)
+        
         # Анімація обертання
         anim = Animation(angle = self.angle + 360, d = 1, t='in_cubic')
         
@@ -108,6 +124,9 @@ class Fish(RotatedImage):
         if not self.anim_play and not self.interaction_block:
             self.hp_current -= 1
             self.GAME_SCREEN.score += 1
+            
+            # Воспроизводим звук клика
+            app.play_sound(app.sound_click)
 
             # Клік призвів до змеьшення hp риби
             if self.hp_current > 0:
@@ -146,12 +165,22 @@ class Game(Screen):
     score = NumericProperty(0)
 
     def on_pre_enter(self, *args):
-        self.score = 0
-        app.LEVEL = 0
-        self.ids.level_complete.opacity = 0
-        self.ids.fish.fish_index = 0
+        if getattr(app, 'returning_to_game_from_settings', False):
+            app.returning_to_game_from_settings = False
+        else:
+            self.score = 0
+            app.LEVEL = 0
+            self.ids.level_complete.opacity = 0
+            self.ids.fish.fish_index = 0
 
         return super().on_pre_enter(*args)
+    
+    # Перехід до екрана налаштувань
+    def go_settings(self, *args):
+        app.previous_screen = "game"
+        self.manager.current = "settings"
+        self.manager.transition.direction = "up"
+
     
     def on_enter(self, *args):
         self.start_game()
@@ -164,6 +193,27 @@ class Game(Screen):
     def level_complete(self, *args):
         # self.ids.level_complete.opacity = 1
         self.ids.level_complete.opacity = 1
+        
+        # Воспроизводим звук завершения уровня
+        app.play_sound(app.sound_level_complete)
+        
+        # Проверяем, есть ли следующий уровень
+        if app.LEVEL + 1 < len(app.LEVELS):
+            # Запланировать переход на следующий уровень через 2 секунды
+            Clock.schedule_once(self.next_level, 2)
+        else:
+            # Игра завершена, планируем возврат в меню через 3 секунды
+            Clock.schedule_once(self.game_over, 3)
+    
+    def next_level(self, *args):
+        app.LEVEL += 1
+        self.ids.level_complete.opacity = 0
+        self.ids.fish.fish_index = 0
+        self.ids.fish.new_fish()
+    
+    def game_over(self, *args):
+        self.manager.current = "menu"
+        self.manager.transition.direction = "right"
 
 
     def go_home(self):
@@ -173,25 +223,42 @@ class Game(Screen):
 
 class ClickerApp(App):
     LEVEL = 0
+    previous_screen = "menu"
+    returning_to_game_from_settings = False
+    volume = NumericProperty(1.0)
 
     FISHES = {
     'fish1':
-        {'source': 'assets/images/fish_01.png', 'hp': 10},
+        {'source': 'assets/images/be3dbadd-9ae5-4882-a7c1-db19071013d7.png', 'hp': 10},
     'fish2':
-        {'source': 'assets/images/fish_02.png', 'hp': 20}
+        {'source': 'assets/images/78971469-ca2f-4739-abf8-a228d9636971.png', 'hp': 20}
     }
 
     LEVELS = [
-        ['fish1', 'fish1', 'fish2']
+        ['fish1', 'fish1', 'fish2'],
+        ['fish1', 'fish2', 'fish2', 'fish1'],
+        ['fish2', 'fish2', 'fish1', 'fish2'],
+        ['fish2', 'fish1', 'fish2', 'fish2', 'fish1']
     ]
 
     def build(self):
+        # Загружаем звуки
+        self.sound_click = SoundLoader.load('assets/audios/bubble01.mp3')
+        self.sound_defeat = SoundLoader.load('assets/audios/fish_def.ogg')
+        self.sound_level_complete = SoundLoader.load('assets/audios/level_complete.ogg')
+        
         sm = ScreenManager()
         sm.add_widget(Menu(name="menu"))
         sm.add_widget(Game(name="game"))
         sm.add_widget(Settings(name="settings"))
 
         return sm
+    
+    def play_sound(self, sound):
+        """Воспроизведение звука с учетом громкости"""
+        if sound:
+            sound.volume = self.volume
+            sound.play()
 
 if platform != 'android':
     Window.size = (450, 900)
